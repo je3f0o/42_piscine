c14f9dc89950fe8c34e21d20c8492842fe63f9ef
je3f0o <je3f0o@gmail.com>
2021-07-22 18:05:58 +0800

    C04 -> 3 ft_atoi.c

---
+++ C04/exercise_03/ft_atoi.c
@@ -0,0 +1,42 @@
+void handle_sign(char ch, int *sign) {
+	if (ch == '-') {
+		*sign *= -1;
+	}
+}
+
+int handle(char ch, int old_value, int *sign, int *is_finished) {
+	if (*is_finished == 1) {
+		return old_value;
+	}
+
+	if (ch <= 32) {
+		return 0;
+	}
+
+	if (ch == '-' || ch == '+') {
+		handle_sign(ch, sign);
+		return old_value;
+	}
+
+	if (ch >= '0' && ch <= '9') {
+		return old_value * 10 + (ch - '0');
+	}
+
+	*is_finished = 1;
+
+	return old_value;
+}
+
+int ft_atoi(char *str) {
+	int i           = 0;
+	int result      = 0;
+	int sign        = 1;
+	int is_finished = 0;
+
+	while (str[i] != 0) {
+		result = handle(str[i], result, &sign, &is_finished);
+		i += 1;
+	}
+
+	return result * sign;
+}
