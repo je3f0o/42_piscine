b46e0938b857f1b4b26f230ac662d3791a774177
je3f0o <je3f0o@gmail.com>
2021-07-22 19:45:30 +0800

    C06 -> 2 ft_rev_params.c

---
+++ C06/exercise_02/ft_rev_params.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int ft_strlen(char *str) {
+	int index = 0;
+
+	while (str[index] != 0) {
+		index += 1;
+	}
+
+	return index;
+}
+
+int main(int argc, char **argv) {
+	int i	= argc - 1;
+	char nl = '\n';
+
+	while (i > 0) {
+		write(1, argv[i], ft_strlen(argv[i]));
+		write(1, &nl, 1);
+		i -= 1;
+	}
+
+	return 0;
+}
