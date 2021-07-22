e80eabaee9c02a48c5797f2279e47475d6f1a458
je3f0o <je3f0o@gmail.com>
2021-07-22 23:20:03 +0800

    C06 -> 3 ft_sort_params.c NORMINETTED.

--- C06/exercise_03/ft_sort_params.c
+++ C06/exercise_03/ft_sort_params.c
@@ -1,69 +1,80 @@
 #include <unistd.h>
 
-int ft_strlen(char *str) {
-	int index = 0;
+int	ft_strlen(char *str)
+{
+	int	index;
 
-	while (str[index] != 0) {
+	index = 0;
+	while (str[index] != 0)
+	{
 		index += 1;
 	}
-
-	return index;
+	return (index);
 }
 
-int ft_strcmp(char *s1, char *s2) {
-	int i		   = 0;
-	int difference = s1[i] - s2[i];
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+	int	difference;
 
-	while ((difference == 0) && (s1[i] != 0)) {
+	i = 0;
+	difference = s1[i] - s2[i];
+	while ((difference == 0) && (s1[i] != 0))
+	{
 		difference = s1[i] - s2[i];
 		i += 1;
 	}
-
-	return difference;
+	return (difference);
 }
 
-void ft_putstr(char *str) {
-	int len = ft_strlen(str);
-	char ch = '\n';
+void	swap_pointers(char **ptrs, int a, int b)
+{
+	char	*str_ptr;
 
-	write(1, str, len);
-	write(1, &ch, 1);
+	str_ptr = ptrs[a];
+	ptrs[a] = ptrs[b];
+	ptrs[b] = str_ptr;
 }
 
-void sort_strings(char **strings, int len) {
-	int i		   = 1;
-	int j		   = 0;
-	int difference = 0;
-	char *temp	   = 0;
+void	sort_strings(char **strings, int len)
+{
+	int		i;
+	int		j;
+	char	*temp;
 
-	while (i < len) {
+	i = 1;
+	j = 0;
+	temp = 0;
+	while (i < len)
+	{
 		j = 1;
-
-		while (j < len) {
-			difference = ft_strcmp(strings[i], strings[j]);
-
-			if (difference < 0) {
-				temp	   = strings[i];
-				strings[i] = strings[j];
-				strings[j] = temp;
+		while (j < len)
+		{
+			if (ft_strcmp(strings[i], strings[j]) < 0)
+			{
+				swap_pointers(strings, i, j);
 			}
-
 			j += 1;
 		}
-
 		i += 1;
 	}
 }
 
-int main(int argc, char **argv) {
-	int i = 1;
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		len;
+	char	ch;
 
+	i = 1;
+	ch = '\n';
 	sort_strings(argv, argc);
-
-	while (i < argc) {
-		ft_putstr(argv[i]);
+	while (i < argc)
+	{
+		len = ft_strlen(argv[i]);
+		write(1, argv[i], len);
+		write(1, &ch, 1);
 		i += 1;
 	}
-
-	return 0;
+	return (0);
 }
