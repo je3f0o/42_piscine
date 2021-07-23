c65dab1abea6b8e22a0b4f547e8c827188c5598e
je3f0o <je3f0o@gmail.com>
2021-07-23 23:26:38 +0800

    C04 -> exercise_03 ft_atoi.c NORMINETTED

---
+++ C04/exercise_03/README.md
@@ -0,0 +1,13 @@
+# C04 exercise_03 ft_atoi.c (NORMINETTED)
+
+Shell:
+```bash
+gcc -Wall -Wextra -Werror main.c ft_atoi.c -o atoi
+norminette ft_atoi.c
+ft_atoi.c: OK!
+
+# Executing
+./atoi
+input string = "      ----+---+1234ab4123".
+i = -1234
+```
--- C04/exercise_03/ft_atoi.c
+++ C04/exercise_03/ft_atoi.c
@@ -1,42 +1,49 @@
-void handle_sign(char ch, int *sign) {
-	if (ch == '-') {
+void	handle_sign(char ch, int *sign)
+{
+	if (ch == '-')
+	{
 		*sign *= -1;
 	}
 }
 
-int handle(char ch, int old_value, int *sign, int *is_finished) {
-	if (*is_finished == 1) {
-		return old_value;
+int	handle(char ch, int old_value, int *sign, int *is_finished)
+{
+	if (*is_finished == 1)
+	{
+		return (old_value);
 	}
-
-	if (ch <= 32) {
-		return 0;
+	if (ch <= 32)
+	{
+		return (0);
 	}
-
-	if (ch == '-' || ch == '+') {
+	if (ch == '-' || ch == '+')
+	{
 		handle_sign(ch, sign);
-		return old_value;
+		return (old_value);
 	}
-
-	if (ch >= '0' && ch <= '9') {
-		return old_value * 10 + (ch - '0');
+	if (ch >= '0' && ch <= '9')
+	{
+		return (old_value * 10 + (ch - '0'));
 	}
-
 	*is_finished = 1;
-
-	return old_value;
+	return (old_value);
 }
 
-int ft_atoi(char *str) {
-	int i           = 0;
-	int result      = 0;
-	int sign        = 1;
-	int is_finished = 0;
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	result;
+	int	sign;
+	int	is_finished;
 
-	while (str[i] != 0) {
+	i = 0;
+	result = 0;
+	sign = 1;
+	is_finished = 0;
+	while (str[i] != 0)
+	{
 		result = handle(str[i], result, &sign, &is_finished);
 		i += 1;
 	}
-
-	return result * sign;
+	return (result * sign);
 }
