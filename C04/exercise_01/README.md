2a0c68fb67b1d5ae8a8490e2258d28cc3399e03a
je3f0o <je3f0o@gmail.com>
2021-07-23 22:44:29 +0800

    C04 -> 01 ft_putstr.c NORMINETTED

---
+++ C04/exercise_01/README.md
@@ -0,0 +1,12 @@
+# C04 exercise_01 ft_putstr.c (NORMINETTED)
+
+Shell:
+```bash
+gcc -Wall -Wextra -Werror main.c ft_putstr.c -o putstr
+norminette ft_putstr.c
+ft_putstr.c: OK!
+
+# Executing
+./putstr
+Hello world
+```
--- C04/exercise_01/ft_putstr.c
+++ C04/exercise_01/ft_putstr.c
@@ -1,16 +1,21 @@
 #include <unistd.h>
 
-int ft_strlen(char *str) {
-	int index = 0;
+int	ft_strlen(char *str)
+{
+	int	i;
 
-	while (str[index] != 0) {
-		index += 1;
+	i = 0;
+	while (str[i] != 0)
+	{
+		i += 1;
 	}
-
-	return index;
+	return (i);
 }
 
-void ft_putstr(char *str) {
-	int len = ft_strlen(str);
+void	ft_putstr(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
 	write(1, str, len);
 }
