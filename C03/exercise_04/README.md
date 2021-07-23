88efc97f374d12490de12d8c89d529bc7854957b
je3f0o <je3f0o@gmail.com>
2021-07-23 17:01:06 +0800

    C03 -> 4 ft_strstr.c NORMINETTED

---
+++ C03/exercise_04/README.md
@@ -0,0 +1,15 @@
+# C03 exercise_04 ft_strstr.c (NORMINETTED)
+
+Shell:
+```bash
+gcc -Wall -Wextra -Werror main.c ft_strstr.c -o strstr
+norminette ft_strstr.c
+ft_strstr.c: OK!
+
+# Executing
+./strstr
+haystack = "This is a simple string.".
+needle   = "simple".
+-------------------------------
+Found "simple" at "simple string.".
+```
--- C03/exercise_04/ft_strstr.c
+++ C03/exercise_04/ft_strstr.c
@@ -1,46 +1,54 @@
-int ft_strlen(char *str) {
-	int index = 0;
+int	ft_strlen(char *str)
+{
+	int	index;
 
-	while (str[index] != 0) {
+	index = 0;
+	while (str[index] != 0)
+	{
 		index += 1;
 	}
-
-	return index;
+	return (index);
 }
 
-int is_found(int offset, char *string, char *to_find, int to_find_length) {
-	int i     = 0;
-	int found = 1;
+int	is_found(int offset, char *string, char *to_find, int to_find_length)
+{
+	int	i;
+	int	found;
 
-	while (i < to_find_length) {
-		if (string[offset + i] != to_find[i]) {
+	i = 0;
+	found = 1;
+	while (i < to_find_length)
+	{
+		if (string[offset + i] != to_find[i])
+		{
 			found = 0;
 		}
-
 		i += 1;
 	}
-
-	return found;
+	return (found);
 }
 
-char *ft_strstr(char *str, char *to_find) {
-	int i              = 0;
-	int found          = 0;
-	int to_find_length = ft_strlen(to_find);
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	found;
+	int	to_find_length;
 
-	if (to_find_length == 0) {
-		return str;
+	i = 0;
+	found = 0;
+	to_find_length = ft_strlen(to_find);
+	if (to_find_length == 0)
+	{
+		return (str);
 	}
-
-	while (str[i] != 0) {
+	while (str[i] != 0)
+	{
 		found = is_found(i, str, to_find, to_find_length);
-
-		if (found == 1) {
-			return &(str[i]);
+		if (found == 1)
+		{
+			return (&(str[i]));
 		}
-
 		i += 1;
 	}
-
-	return 0;
+	return (0);
 }
